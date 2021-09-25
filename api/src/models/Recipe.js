fcbb96ca337bb674bc8f538440f1e0db8efeaf0b
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-09-25 16:02:23 -0300

    .

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,11 +1,18 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define("recipe", {
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  sequelize.define("diet", {
+    name: {
+      type: DataTypes.TEXT,
       allowNull: false,
     },
   });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,34 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.get("/recipes", (req, res) => {
+  res.status(200).json({
+    msg: "Recipes with query",
+    desc: "Get a list of recipes with a query parameter if dosen't exist show a message",
+  });
+});
+router.get("/recipes/:idRecipe", (req, res) => {
+  res
+    .status(200)
+    .json({ msg: "Recipes with ID", desc: "Get details about a recipe" });
+});
+router.get("/types", (req, res) => {
+  res.status(200).json({
+    msg: "Types",
+    desc: "Get all the types of diet or return from spoonacular if dosen't exist",
+  });
+});
+router.post("/recipe", (req, res) => {
+  res.status(200).json({
+    msg: "Recipe",
+    desc: "Get data from controlled form and create a new recipe",
+  });
+});
 
 module.exports = router;
